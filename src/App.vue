db58e4eea3544e08df23d951335a44ce8df56046
Александр Иванович Самаркин <alexsamarkinru@gmail.com>
2022-03-08 05:35:28 +0300

    comp2_final

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@ import comp2 from './components/comp2.vue';
   <comp1 header1='Этот заголовок N1 задан параметром' />
   <comp1 header1='Этот заголовок N2 задан параметром' header2='Подзаголовок, задан вторым параметром'/>
   
-  <comp2 />
+  <comp2 title='Расчет цены 1' price2='10' />
+  <comp2 title='Расчет цены 2' price2='20' />
 </template>
 
 <style>
--- src/components/comp2.vue
+++ src/components/comp2.vue
@@ -1,13 +1,41 @@
 <script setup>
+import { reactive, computed, ref } from 'vue'
 
+const comp2 = reactive({
+  nameof: 'Товар',
+  price: 125,
+  count: 84,
+});
+
+defineProps(['title','price2'])
+
+const overall = computed(() => {
+  return comp2.price*comp2.count;
+})
 </script>
 
 <template>
-    <div class='compyted'>
-        <p>Компонент с рассчитываемым полем</p>
+    <div class='computed'>
+        <h3>{{title}} {{ overall2 }}</h3>
+        <hr />
+        <p>Наименование товара - {{comp2.nameof}}, количество - {{comp2.count}} шт.</p>
+        <div class='result'> Цена за шт. - {{comp2.price}} руб., стоимость - {{ overall }} руб.</div>
+        <p>Расчет стоимости выражением {{ comp2.price*comp2.count }}</p>
     </div>
 </template>
 
-<style>
-
+<style scoped>
+    .computed {
+        background-color :aliceblue;
+        color :black;
+        margin-left: 50px;
+        border : solid;
+    }
+    .computed .result {
+        background-color : white;
+        color:brown;
+        border: solid;
+        width : 50%;
+    }
+    
 </style>
